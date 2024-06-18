182d2a8609ae0f03ce6d6f55a565a3fc0237aefd
Vivek Yadav <vivekcode101@gmail.com>
2024-06-18 17:34:45 +0530

    Accounts added in the db
    Signed-off-by: Vivek Yadav <vivekcode101@gmail.com>

--- db/query/account.sql
+++ db/query/account.sql
@@ -5,5 +5,14 @@ INSERT INTO accounts (
   currency
 ) VALUES (
     $1, $2, $3
-)
-RETURNING *;
+) RETURNING *;
+
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAccounts :many
+SELECT * FROM accounts
+ORDER BY id
+LIMIT $1
+OFFSET $2;
